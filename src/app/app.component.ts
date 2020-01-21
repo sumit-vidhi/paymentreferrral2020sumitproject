6eca017cc87682977764c939425bb40a5d81f0ba
jagjeetcse78 <jagjeetcse78@gmail.com>
2020-01-21 09:58:42 -0800

    Addnew

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { appSettings } from '@configs/app-settings.config';
 import { AuthService } from '@modules/auth/services/auth.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 interface User {
   userId: number;
@@ -29,11 +30,17 @@ export class AppComponent implements OnInit {
   title = appSettings.appTitle;
   logo = appSettings.appLogo;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
 
   }
 
   ngOnInit() {
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0)
+    });
 
   }
 }
